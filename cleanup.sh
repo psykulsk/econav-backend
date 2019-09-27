8cb58151e069fd38dea82ca5c19f3933ecea6fa5
Piotr Sykulski <pitersk@gmail.com>
2019-09-28 00:02:03 +0200

    Fix cleanup.sh. Add stack to manifest. Change runtime to python-3

--- cleanup.sh
+++ cleanup.sh
@@ -6,4 +6,6 @@
 . ./servicenames.sh
 
 # Delete AppID
+ibmcloud resource service-binding-delete $AppID_service $AppID_service
+ibmcloud resource service-alias-delete $AppID_service -g hackzurich
 ibmcloud resource service-instance-delete $AppID_service
