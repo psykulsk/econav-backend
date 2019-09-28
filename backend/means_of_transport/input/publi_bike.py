fe8d2812d9bb5a4878143e81243b2dbe5375357d
Jakub-Golinowski <jakub.golinowski@gmail.com>
2019-09-28 11:53:33 +0200

    Move to the direct publi_bike API

--- backend/means_of_transport/input/publi_bike.py
+++ backend/means_of_transport/input/publi_bike.py
@@ -6,16 +6,20 @@ from means_of_transport.personal_transport import PersonalTransport
 from means_of_transport.transport_type_mapping import TRANSPORT_TYPE_TO_OUTPUT_TYPE
 from transport_type import TransportType
 
-PUBLI_BIKE_GET_ENDPOINT = \
+# deprecated -> we move to publibike direct API
+STADT_ZURICH_ENDPOINT = \
     'https://data.stadt-zuerich.ch/dataset/d8951db3-4df1-483d-af1e-b34211c63ee9/resource/' \
     '91d89d8b-bfb4-4ca0-a2dd-3dbce11817c4/download/publibike.json'
+
+PUBLI_BIKE_GET_ENDPOINT="https://api.publibike.ch/v1/public/stations"
 
 
 def publi_bike_to_personal_transport(publi_bike_station):
     type = TransportType.BIKE
     company = 'publi_bike'
-    long = publi_bike_station["geometry"]["coordinates"][0]
-    lat = publi_bike_station["geometry"]["coordinates"][1]
+    lat = publi_bike_station["latitude"]
+    long = publi_bike_station["longitude"]
+
     return PersonalTransport(type=type, company=company, long=long, lat=lat)
 
 
@@ -26,7 +30,7 @@ def get_publi_bike_stations():
         return []
     else:
         response_json = json.loads(response.text)
-        publi_bike_response_stations = response_json['features']
+        publi_bike_response_stations = response_json
         personal_transport_list = []
         for publi_bike_station in publi_bike_response_stations:
             personal_transport_list.append(
