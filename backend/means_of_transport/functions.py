515d444435ab14ec770f6248ff423280715b908a
Jakub-Golinowski <jakub.golinowski@gmail.com>
2019-09-28 10:30:56 +0200

    Add publi_bike to neighbourhood response

--- backend/means_of_transport/functions.py
+++ backend/means_of_transport/functions.py
@@ -1,10 +1,12 @@
 from means_of_transport.personal_transport import PersonalTransport
 from means_of_transport.input.flash import get_flash_scooters
+from means_of_transport.input.publi_bike import get_publi_bike_stations
 
 
 def get_personal_transport_list(user_long, user_lat):
     personal_transport_list = []
     personal_transport_list.extend(get_flash_scooters(user_long=user_long, user_lat=user_lat))
+    personal_transport_list.extend(get_publi_bike_stations())
     return personal_transport_list
 
 
@@ -15,4 +17,5 @@ def get_personal_transport_output_list(user_long, user_lat):
 
 if __name__ == '__main__':
     out = get_personal_transport_output_list(8.55, 47.3)
+    print(out)
     pass
